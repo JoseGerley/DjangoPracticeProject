19b48642b225bc86b2488ad13c50e54af27eefbd
JoseGerley <1193260391@u.icesi.edu.co>
2022-06-08 22:38:47 -0500

    Add views concept on Django

--- polls/views.py
+++ polls/views.py
@@ -2,6 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Estas en la pagina principal de Premios Platzi App.")
 
-# Create your views here.
+
+def detail(request, question_id):
+    return HttpResponse(f"Estas viendo la pregunta número {question_id}.")
+
+
+def results(request, question_id):
+    return HttpResponse(f"Estas viendo los resultados de la pregunta número {question_id}.")
+
+def vote(request, question_id):
+    return HttpResponse(f"Estas votando en la pregunta número {question_id}.")
