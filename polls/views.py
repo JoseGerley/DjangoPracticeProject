b0b40d4b2e2243ed3465861ff076bbc9a937f902
JoseGerley <1193260391@u.icesi.edu.co>
2022-06-09 00:01:27 -0500

    Add templates and changes on view for polls app

--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
-
+from .models import Question
 def index(request):
-    return HttpResponse("Estas en la pagina principal de Premios Platzi App.")
+    lastest_question_list = Question.objects.all()
+    return render(request, 'polls/index.html',{'lastest_question_list': lastest_question_list})
 
 
 def detail(request, question_id):
-    return HttpResponse(f"Estas viendo la pregunta número {question_id}.")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
