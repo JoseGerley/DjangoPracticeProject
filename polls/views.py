92ee9588e5d503a3821ca872392ef0e085f65368
JoseGerley <1193260391@u.icesi.edu.co>
2022-06-09 00:21:11 -0500

    Add view and form to vote for the questions

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,8 @@
+from multiprocessing import reduction
 from django.shortcuts import render,get_object_or_404
-from django.http import HttpResponse
-from .models import Question
+from django.urls import reverse
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Question,Choice
 def index(request):
     lastest_question_list = Question.objects.all()
     return render(request, 'polls/index.html',{'lastest_question_list': lastest_question_list})
@@ -15,4 +17,14 @@ def results(request, question_id):
     return HttpResponse(f"Estas viendo los resultados de la pregunta número {question_id}.")
 
 def vote(request, question_id):
-    return HttpResponse(f"Estas votando en la pregunta número {question_id}.")
+    question = get_object_or_404(Question,pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, "polls/detail.html",
+                      {'question':question,
+                       'error_message':"No elegiste una respuesta, por favor escoge una"})
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results",args=(question_id,)))
